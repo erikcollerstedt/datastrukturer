c2738319ae1114a914f2b9d87f6bc8729ef774b2
Erik Collerstedt <eken97@hotmail.com>
2016-01-20 08:25:23 +0100

    Fixed som tests.

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -2,11 +2,29 @@ import unittest
 from exercises.simple import Stack, Queue
 
 class StackTests(unittest.TestCase):
-    def test_init(self):
-    	stack = stack()
+	def test_is_empty(self):
+		s = Stack()
+		self.assertEqual(s.is_empty(), True)
 
-    	self.assertEqual(self.data[])
+		s.push(2)
+		self.assertEqual(s.is_empty(), False)
 
+		s.pop()
+		self.assertEqual(s.is_empty(), True)
+
+	def test_size(self):
+		s = Stack()
+
+		s.push(2)
+		self.assertEqual(s.size(), 1)
+
+	def test_pop(self):
+		pass
+
+	def test_peek(self):
+		s = Stack()
+
+		self.assersRaises(EmptyStack, stack.peek)
 
 
 class QueueTests(unittest.TestCase):
